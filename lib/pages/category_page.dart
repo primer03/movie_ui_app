d8c79f1aa76e38a9de860a4dc63b983dc3d5fab1
primer03 <Panupong.person@gmail.com>
2024-08-22 20:44:19 +0700

    Refactor code for better readability and maintainability in NovelDetail page

--- lib/pages/category_page.dart
+++ lib/pages/category_page.dart
@@ -2,16 +2,14 @@ import 'package:bloctest/bloc/novelcate/novel_cate_bloc.dart';
 import 'package:bloctest/function/app_function.dart';
 import 'package:bloctest/main.dart';
 import 'package:bloctest/models/novel_model.dart';
-import 'package:bloctest/widgets/ContainerSkeltion.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:loading_animation_widget/loading_animation_widget.dart';
-import 'package:scroll_to_index/scroll_to_index.dart';
 import 'package:shimmer/shimmer.dart';
-import 'package:vertical_scrollable_tabview/vertical_scrollable_tabview.dart';
+import 'package:scroll_to_index/scroll_to_index.dart';
 
 class CategoryPage extends StatefulWidget {
   const CategoryPage({super.key, required this.cateId, required this.cate});
@@ -24,30 +22,28 @@ class CategoryPage extends StatefulWidget {
 
 class _CategoryPageState extends State<CategoryPage>
     with TickerProviderStateMixin {
-  // final List<String> data = List.generate(10, (index) => 'Category $index');
   late TabController tabController;
   late AutoScrollController autoScrollController;
   final ScrollController scrollController = ScrollController();
 
-  bool isMenuVisible = true; // State to manage visibility
+  bool isMenuVisible = true;
 
   @override
   void initState() {
     super.initState();
     tabController = TabController(length: widget.cate.length, vsync: this);
     autoScrollController = AutoScrollController();
-    // tabController ไปยัง cateId ที่เลือก
     tabController.animateTo(widget.cateId);
     scrollController.addListener(() {
       if (scrollController.position.userScrollDirection ==
           ScrollDirection.reverse) {
         setState(() {
-          isMenuVisible = false; // Show menu when scrolling down
+          isMenuVisible = false;
         });
       } else if (scrollController.position.userScrollDirection ==
           ScrollDirection.forward) {
         setState(() {
-          isMenuVisible = true; // Hide menu when scrolling up
+          isMenuVisible = true;
         });
       }
     });
@@ -87,13 +83,10 @@ class _CategoryPageState extends State<CategoryPage>
           ),
         ],
         bottom: PreferredSize(
-          preferredSize: Size.fromHeight(50.0), // กำหนดความสูงของ bottom
+          preferredSize: Size.fromHeight(50.0),
           child: Container(
             decoration: BoxDecoration(
               color: Colors.white,
-              // border: Border(
-              //   top: BorderSide(color: Colors.grey[300]!, width: 2),
-              // ),
             ),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -126,7 +119,6 @@ class _CategoryPageState extends State<CategoryPage>
                   decoration: BoxDecoration(
                       border: Border(
                     bottom: BorderSide(color: Colors.grey[300]!, width: 2),
-                    // left: BorderSide(color: Colors.grey[300]!, width: 2),
                   )),
                   child: IconButton(
                     icon: const Icon(Icons.keyboard_arrow_down_rounded),
@@ -145,25 +137,9 @@ class _CategoryPageState extends State<CategoryPage>
             ),
           ),
         ),
-        // bottom: TabBar(
-        //   tabAlignment: TabAlignment.center,
-        //   controller: tabController,
-        //   isScrollable: true,
-        //   labelColor: Colors.black,
-        //   labelStyle: GoogleFonts.athiti(
-        //     fontSize: 16,
-        //     fontWeight: FontWeight.bold,
-        //   ),
-        //   indicatorColor: Colors.black,
-        //   tabs: data
-        //       .map((e) => Tab(
-        //             text: e,
-        //           ))
-        //       .toList(),
-        // ),
       ),
       body: BlocBuilder<NovelCateBloc, NovelCateState>(
-        builder: (context, NovelCateState state) {
+        builder: (context, state) {
           if (state is NovelCateLoading) {
             return Center(
               child: LoadingAnimationWidget.discreteCircle(
@@ -174,159 +150,143 @@ class _CategoryPageState extends State<CategoryPage>
               ),
             );
           } else if (state is NovelCateLoaded) {
-            // add ค่าเปล่าเข้าไปเพื่อให้มันเริ่มที่ 0
             return Stack(
               children: [
-                SingleChildScrollView(
+                ListView.builder(
                   controller: scrollController,
-                  child: Column(
-                    children: [
-                      const SizedBox(height: 50),
-                      ...state.allsearch.map(
-                        (e) => Card(
-                          elevation: 0,
-                          color: Colors.white,
-                          margin: EdgeInsets.only(
-                            left: 20,
-                            right: 20,
-                            bottom: 20,
-                          ),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Container(
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(10),
-                                  boxShadow: [
-                                    BoxShadow(
-                                      color: Colors.black.withOpacity(0.5),
-                                      blurRadius: 6,
-                                      spreadRadius: 0.1,
-                                      offset: const Offset(4, 5),
-                                    ),
-                                  ],
+                  itemCount: state.allsearch.length,
+                  itemBuilder: (context, index) {
+                    final e = state.allsearch[index];
+                    return Card(
+                      elevation: 0,
+                      color: Colors.white,
+                      margin: EdgeInsets.only(
+                        left: 20,
+                        right: 20,
+                        bottom: 20,
+                        top: index == 0 ? 20 : 0,
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.black.withOpacity(0.5),
+                                  blurRadius: 6,
+                                  spreadRadius: 0.1,
+                                  offset: const Offset(4, 5),
                                 ),
-                                child: ClipRRect(
-                                  borderRadius: BorderRadius.circular(10),
-                                  // child: Image.network(
-                                  //   e.img,
-                                  //   height: 140,
-                                  //   fit: BoxFit.cover,
-                                  // ),
-
-                                  child: CachedNetworkImage(
-                                    imageUrl: e.img,
-                                    fit: BoxFit.fill,
+                              ],
+                            ),
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(10),
+                              child: CachedNetworkImage(
+                                imageUrl: e.img,
+                                fit: BoxFit.fill,
+                                height: 140,
+                                placeholder: (context, url) =>
+                                    Shimmer.fromColors(
+                                  baseColor: Colors.grey[400]!,
+                                  highlightColor: Colors.grey[100]!,
+                                  period: const Duration(milliseconds: 1000),
+                                  child: Container(
                                     height: 140,
-                                    placeholder: (context, url) =>
-                                        Shimmer.fromColors(
-                                      baseColor: Colors.grey[400]!,
-                                      highlightColor: Colors.grey[100]!,
-                                      period:
-                                          const Duration(milliseconds: 1000),
-                                      child: Container(
-                                        height: 140,
-                                        width: 100,
-                                        decoration: BoxDecoration(
-                                          color: Colors.grey[200],
-                                          borderRadius:
-                                              BorderRadius.circular(10),
-                                        ),
-                                      ),
+                                    width: 100,
+                                    decoration: BoxDecoration(
+                                      color: Colors.grey[200],
+                                      borderRadius: BorderRadius.circular(10),
                                     ),
                                   ),
                                 ),
                               ),
-                              const SizedBox(width: 15),
-                              Expanded(
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
+                            ),
+                          ),
+                          const SizedBox(width: 15),
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  e.name,
+                                  style: GoogleFonts.athiti(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                const SizedBox(height: 5),
+                                Text(
+                                  e.title,
+                                  maxLines: 3,
+                                  overflow: TextOverflow.ellipsis,
+                                  style: GoogleFonts.athiti(
+                                    fontSize: 14,
+                                    color: Colors.grey[500],
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                ),
+                                const SizedBox(height: 5),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.start,
                                   children: [
-                                    Text(
-                                      e.name,
-                                      style: GoogleFonts.athiti(
-                                        fontSize: 16,
-                                        fontWeight: FontWeight.bold,
-                                      ),
+                                    const Icon(
+                                      Icons.remove_red_eye,
+                                      size: 16,
                                     ),
-                                    const SizedBox(height: 5),
+                                    const SizedBox(width: 5),
                                     Text(
-                                      e.title,
-                                      maxLines: 3,
-                                      overflow: TextOverflow.ellipsis,
+                                      abbreviateNumber(e.view),
                                       style: GoogleFonts.athiti(
                                         fontSize: 14,
-                                        color: Colors.grey[500],
+                                        color: Colors.grey,
                                         fontWeight: FontWeight.w600,
                                       ),
                                     ),
-                                    const SizedBox(height: 5),
-                                    Row(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.start,
-                                      children: [
-                                        const Icon(
-                                          Icons.remove_red_eye,
-                                          size: 16,
-                                        ),
-                                        const SizedBox(width: 5),
-                                        Text(
-                                          abbreviateNumber(e.view),
-                                          style: GoogleFonts.athiti(
-                                            fontSize: 14,
-                                            color: Colors.grey,
-                                            fontWeight: FontWeight.w600,
-                                          ),
-                                        ),
-                                        const SizedBox(width: 10),
-                                        const Icon(
-                                          Icons.list,
-                                          size: 16,
-                                        ),
-                                        const SizedBox(width: 5),
-                                        Text(
-                                          abbreviateNumber(e.allep),
-                                          style: GoogleFonts.athiti(
-                                            fontSize: 14,
-                                            color: Colors.grey,
-                                            fontWeight: FontWeight.w600,
-                                          ),
-                                        ),
-                                        const SizedBox(width: 10),
-                                        const Icon(
-                                          Icons.thumb_up,
-                                          size: 16,
-                                        ),
-                                        const SizedBox(width: 5),
-                                        Text(
-                                          abbreviateNumber(e.score),
-                                          style: GoogleFonts.athiti(
-                                            fontSize: 14,
-                                            color: Colors.grey,
-                                            fontWeight: FontWeight.w600,
-                                          ),
-                                        ),
-                                      ],
+                                    const SizedBox(width: 10),
+                                    const Icon(
+                                      Icons.list,
+                                      size: 16,
+                                    ),
+                                    const SizedBox(width: 5),
+                                    Text(
+                                      abbreviateNumber(e.allep),
+                                      style: GoogleFonts.athiti(
+                                        fontSize: 14,
+                                        color: Colors.grey,
+                                        fontWeight: FontWeight.w600,
+                                      ),
+                                    ),
+                                    const SizedBox(width: 10),
+                                    const Icon(
+                                      Icons.thumb_up,
+                                      size: 16,
+                                    ),
+                                    const SizedBox(width: 5),
+                                    Text(
+                                      abbreviateNumber(e.score),
+                                      style: GoogleFonts.athiti(
+                                        fontSize: 14,
+                                        color: Colors.grey,
+                                        fontWeight: FontWeight.w600,
+                                      ),
                                     ),
                                   ],
                                 ),
-                              ),
-                            ],
+                              ],
+                            ),
                           ),
-                        ),
+                        ],
                       ),
-                    ],
-                  ),
+                    );
+                  },
                 ),
-                // Menu collapse with slide down animation
                 AnimatedSlide(
-                  offset: isMenuVisible
-                      ? Offset(0, 0)
-                      : Offset(0, -1), // Slide down from top
-                  duration: const Duration(
-                      milliseconds: 300), // Duration of animation
-                  curve: Curves.easeInOut, // Smooth transition curve
+                  offset: isMenuVisible ? Offset(0, 0) : Offset(0, -1),
+                  duration: const Duration(milliseconds: 300),
+                  curve: Curves.easeInOut,
                   child: Container(
                     height: 50,
                     padding: const EdgeInsets.symmetric(horizontal: 20),
@@ -341,7 +301,6 @@ class _CategoryPageState extends State<CategoryPage>
                             fontWeight: FontWeight.bold,
                           ),
                         ),
-                        //dropdown
                       ],
                     ),
                   ),
