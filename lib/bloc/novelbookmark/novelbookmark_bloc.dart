7e57fe4bd9b3ec93bdc2633b72f4e03859e8d8ea
primer03 <panupong.person@gmail.com>
2024-09-02 09:00:01 +0700

    Refactor NovelbookmarkBloc to handle adding and removing bookmarks

--- lib/bloc/novelbookmark/novelbookmark_bloc.dart
+++ lib/bloc/novelbookmark/novelbookmark_bloc.dart
@@ -1,5 +1,6 @@
 import 'package:bloc/bloc.dart';
 import 'package:bloctest/models/novel_bookmark_model.dart';
+import 'package:bloctest/models/novel_detail_model.dart';
 import 'package:bloctest/repositories/novel_repository.dart';
 import 'package:equatable/equatable.dart';
 part 'novelbookmark_event.dart';
@@ -8,6 +9,9 @@ part 'novelbookmark_state.dart';
 class NovelbookmarkBloc extends Bloc<NovelbookmarkEvent, NovelbookmarkState> {
   NovelbookmarkBloc() : super(NovelbookmarkInitial()) {
     on<FetchBookmark>(_onFetchBookmark);
+    on<AddBookmarkEvent>(_onAddBookmark);
+    on<RemoveBookmarkEvent>(_onRemoveBookmark);
+    on<BoolmarkSearchEvent>(_onsearchBookmark);
   }
 
   NovelRepository novelRepository = NovelRepository();
@@ -17,6 +21,44 @@ class NovelbookmarkBloc extends Bloc<NovelbookmarkEvent, NovelbookmarkState> {
     emit(BookmarkLoading());
     try {
       final bookmarkList = await novelRepository.getBookmark();
+      bookmarkList.isEmpty
+          ? emit(BookmarkEmpty())
+          : emit(BookmarkLoaded(bookmarkList));
+    } catch (e) {
+      emit(BookmarkError(e.toString()));
+    }
+  }
+
+  void _onAddBookmark(
+      AddBookmarkEvent event, Emitter<NovelbookmarkState> emit) async {
+    try {
+      final bookmarkList = await novelRepository.getBookmark();
+      bookmarkList.isEmpty
+          ? emit(BookmarkEmpty())
+          : emit(BookmarkLoaded(bookmarkList));
+    } catch (e) {
+      emit(BookmarkError(e.toString()));
+    }
+  }
+
+  void _onRemoveBookmark(
+      RemoveBookmarkEvent event, Emitter<NovelbookmarkState> emit) async {
+    try {
+      final bookmarkList = await novelRepository.getBookmark();
+      bookmarkList.isEmpty
+          ? emit(BookmarkEmpty())
+          : emit(BookmarkLoaded(bookmarkList));
+    } catch (e) {
+      emit(BookmarkError(e.toString()));
+    }
+  }
+
+  void _onsearchBookmark(
+      BoolmarkSearchEvent event, Emitter<NovelbookmarkState> emit) async {
+    emit(BookmarlSearching(event.bookmarkList));
+    try {
+      final bookmarkList =
+          await novelRepository.searchBookmark(event.cateId, event.search);
       bookmarkList.isEmpty
           ? emit(BookmarkEmpty())
           : emit(BookmarkLoaded(bookmarkList));
