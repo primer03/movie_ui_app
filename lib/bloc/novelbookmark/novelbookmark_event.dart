8d0b3ba8115b28c586f55c60612327c936cbc9f6
primer03 <panupong.person@gmail.com>
2024-09-02 09:00:04 +0700

    feat: Add bookmark events and sorting functionality

--- lib/bloc/novelbookmark/novelbookmark_event.dart
+++ lib/bloc/novelbookmark/novelbookmark_event.dart
@@ -13,3 +13,39 @@ final class FetchBookmark extends NovelbookmarkEvent {
   @override
   List<Object> get props => [];
 }
+
+final class AddBookmarkEvent extends NovelbookmarkEvent {
+  const AddBookmarkEvent();
+
+  @override
+  List<Object> get props => [];
+}
+
+final class RemoveBookmarkEvent extends NovelbookmarkEvent {
+  const RemoveBookmarkEvent();
+
+  @override
+  List<Object> get props => [];
+}
+
+final class BoolmarkSearchEvent extends NovelbookmarkEvent {
+  final String? search;
+  final String? cateId;
+  final List<Bookmark> bookmarkList;
+
+  const BoolmarkSearchEvent(
+      {this.search, this.cateId, required this.bookmarkList});
+
+  @override
+  List<Object> get props => [search ?? '', cateId ?? '', bookmarkList];
+}
+
+final class BookmarkSortEvent extends NovelbookmarkEvent {
+  final List<Bookmark> bookmarkList;
+  final String sort;
+
+  const BookmarkSortEvent({required this.bookmarkList, required this.sort});
+
+  @override
+  List<Object> get props => [bookmarkList, sort];
+}
