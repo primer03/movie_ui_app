9eeb2896620b063c9e584a6d979f2309182281fb
primer03 <panupong.person@gmail.com>
2024-09-03 17:55:54 +0700

    feat: Add ReadnovelBloc for reading novels

---
+++ lib/bloc/novelread/readnovel_bloc.dart
@@ -0,0 +1,31 @@
+import 'package:bloc/bloc.dart';
+import 'package:bloctest/models/novel_read_model.dart';
+import 'package:bloctest/repositories/novel_repository.dart';
+import 'package:equatable/equatable.dart';
+
+part 'readnovel_event.dart';
+part 'readnovel_state.dart';
+
+class ReadnovelBloc extends Bloc<ReadnovelEvent, ReadnovelState> {
+  ReadnovelBloc() : super(ReadnovelInitial()) {
+    on<FetchReadnovel>(_onFetchNoveReadl);
+  }
+
+  NovelRepository novelRepository = NovelRepository();
+
+  void _onFetchNoveReadl(
+      FetchReadnovel event, Emitter<ReadnovelState> emit) async {
+    emit(ReadnovelLoading());
+    try {
+      BookfetNovelRead bookfetNovelRead =
+          await novelRepository.getReadNovel(event.bookId, event.epId);
+      if (bookfetNovelRead.readnovel.novelBook.id == 0) {
+        emit(ReadnovelNoData());
+      } else {
+        emit(ReadnovelLoaded(bookfetNovelRead: bookfetNovelRead));
+      }
+    } catch (e) {
+      emit(const ReadnovelError(message: 'ไม่สามารถโหลดข้อมูลได้'));
+    }
+  }
+}
