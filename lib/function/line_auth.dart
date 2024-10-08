63ab7dcdcf57e9d182e8e9c6daec38d379345d17
primer03 <panupong.person@gmail.com>
2024-10-08 15:22:27 +0700

    feat: Add lazy loading for images in line_auth.dart

--- lib/function/line_auth.dart
+++ lib/function/line_auth.dart
@@ -1,6 +1,11 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_line_sdk/flutter_line_sdk.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:http/http.dart' as http;
+import 'package:path/path.dart' as path;
 
 void lineSDKInit() async {
   await LineSDK.instance.setup("2002469697").then((_) {
@@ -17,7 +22,7 @@ Future getAccessToken() async {
   }
 }
 
-void startLineLogin(BuildContext context) async {
+Future<String> startLineLogin(BuildContext context) async {
   try {
     final result = await LineSDK.instance.login(scopes: ["profile"]);
     // Navigator.of(context).pop();
@@ -33,6 +38,7 @@ void startLineLogin(BuildContext context) async {
     print("StatusMessage> " + statusmessage!);
     print("ProfileURL> " + imgUrl!);
     print("userId> " + userId!);
+    return downloadImage(imgUrl);
   } on PlatformException catch (e) {
     print(e);
     switch (e.code.toString()) {
@@ -46,6 +52,7 @@ void startLineLogin(BuildContext context) async {
         print("Unknown but failed to login");
         break;
     }
+    return '';
   }
 }
 
@@ -54,5 +61,49 @@ void logoutLine() async {
     await LineSDK.instance.logout();
   } on PlatformException catch (e) {
     print(e.message);
+  }
+}
+
+Future<String> downloadImage(String imageUrl) async {
+  try {
+    final response = await http.get(Uri.parse(imageUrl));
+
+    if (response.statusCode == 200) {
+      final contentType = response.headers['content-type'];
+      String fileExtension = '.jpg'; // Default extension
+
+      if (contentType != null) {
+        switch (contentType.toLowerCase()) {
+          case 'image/jpeg':
+            fileExtension = '.jpg';
+            break;
+          case 'image/png':
+            fileExtension = '.png';
+            break;
+          case 'image/gif':
+            fileExtension = '.gif';
+            break;
+          case 'image/webp':
+            fileExtension = '.webp';
+            break;
+          // Add more cases as needed
+        }
+      }
+
+      final directory = await getApplicationDocumentsDirectory();
+      final fileName = 'downloaded_image$fileExtension';
+      final filePath = path.join(directory.path, fileName);
+
+      final file = File(filePath);
+      await file.writeAsBytes(response.bodyBytes);
+      return filePath;
+      // print('Image saved at $filePath');
+    } else {
+      print('Failed to load image: ${response.statusCode}');
+      return '';
+    }
+  } catch (e) {
+    print('Error downloading image: $e');
+    return '';
   }
 }
