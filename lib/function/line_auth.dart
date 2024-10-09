9f61e5d9587b061e6d2fb61c5266634269fd7416
primer03 <panupong.person@gmail.com>
2024-10-09 17:53:10 +0700

    feat: Add lazy loading for images in line_auth.dart

--- lib/function/line_auth.dart
+++ lib/function/line_auth.dart
@@ -1,14 +1,20 @@
 import 'dart:io';
 
+import 'package:bloctest/bloc/novel/novel_bloc.dart';
 import 'package:bloctest/function/app_function.dart';
+import 'package:bloctest/main.dart';
+import 'package:bloctest/pages/auth/social_last_regis.dart';
 import 'package:bloctest/repositories/user_repository.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_line_sdk/flutter_line_sdk.dart';
+import 'package:jwt_decoder/jwt_decoder.dart';
 import 'package:logger/web.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:http/http.dart' as http;
 import 'package:path/path.dart' as path;
+import 'package:toastification/toastification.dart';
 
 void lineSDKInit() async {
   await LineSDK.instance.setup("2002469697").then((_) {
@@ -25,7 +31,7 @@ Future getAccessToken() async {
   }
 }
 
-Future<String?> startLineLogin(BuildContext context) async {
+Future<void> startLineLogin(BuildContext context) async {
   try {
     final result =
         await LineSDK.instance.login(scopes: ["profile", "openid", "email"]);
@@ -38,27 +44,117 @@ Future<String?> startLineLogin(BuildContext context) async {
     var userId = result.userProfile?.userId;
     var email = result.accessToken.email;
 
-    print("AccessToken> " + accesstoken);
-    print("DisplayName> " + displayname!);
-    print("StatusMessage> " + statusmessage!);
-    print("ProfileURL> " + imgUrl!);
-    print("userId> " + userId!);
-    print("email> " + email!);
+    // print("AccessToken> " + accesstoken);
+    // print("DisplayName> " + displayname!);
+    // print("StatusMessage> " + statusmessage!);
+    // print("ProfileURL> " + imgUrl!);
+    // print("userId> " + userId!);
+    // print("email> " + email!);
     UserRepository userRepository = UserRepository();
-    // var user = await userRepository.registerUser(
-    //   username: displayname,
-    //   email: email ?? userId,
-    //   password: userId,
-    //   date: '2021-10-10',
-    //   gender: 'M',
-    // );
-    await userRepository.loginUser(
-      email: email,
-      password: userId,
-      identifier: await getDevice(),
-    );
+    try {
+      if (email != null) {
+        bool check = await userRepository.registersocial(
+          username: displayname!,
+          email: email,
+          identifier: await getDevice(),
+          firstRegis: true,
+        );
+        if (check) {
+          final usertokenHive = await novelBox.get('usertoken');
+          final user = JwtDecoder.decode(usertokenHive);
 
-    return downloadImage(imgUrl);
+          print('user: ${user['detail']['gender']}');
+          if (user['detail']['gender'] == null) {
+            String downloadedImagePath = '';
+            if (imgUrl != null) {
+              downloadedImagePath = await downloadImage(imgUrl);
+            }
+            Navigator.push(context, MaterialPageRoute(builder: (context) {
+              return SocialLastRegis(
+                displayName: displayname,
+                email: email,
+                imgUrl: downloadedImagePath,
+                userId: userId!,
+              );
+            }));
+          }
+
+          // final user = await novelBox.get('user');
+          // await novelBox.put('loginType', 'normal');
+          // await novelBox.put('socialType', 'line');
+          // await novelBox.put('issocial', true);
+          // if (imgUrl != null) {
+          //   await userRepository.updateImageUser(
+          //     image: File(await downloadImage(imgUrl)),
+          //     type: 'social',
+          //   );
+          // }
+          // Logger().i('user: $user');
+          // showToastification(
+          //   context: context,
+          //   message: 'กำลังเข้าสู่ระบบ โปรดรอสักครู่',
+          //   type: ToastificationType.info,
+          //   style: ToastificationStyle.minimal,
+          // );
+          // Navigator.push(context, MaterialPageRoute(builder: (context) {
+          //   return SocialLastRegis(
+          //     displayName: displayname!,
+          //     email: email,
+          //     imgUrl: downloadedImagePath,
+          //     userId: userId!,
+          //   );
+          // }));
+        }
+        // String check = await userRepository.loginCheckSocial(
+        //   email: email,
+        //   password: '',
+        //   identifier: await getDevice(),
+        // );
+        // print('check: $check');
+        // if (check == 'ไม่พบผู้ใช้หรืออีเมลนี้ในระบบ') {
+        //   String downloadedImagePath = '';
+        //   if (imgUrl != null) {
+        //     downloadedImagePath = await downloadImage(imgUrl);
+        //   }
+        //   Navigator.push(context, MaterialPageRoute(builder: (context) {
+        //     return SocialLastRegis(
+        //       displayName: displayname!,
+        //       email: email,
+        //       imgUrl: downloadedImagePath,
+        //       userId: userId!,
+        //     );
+        //   }));
+        // } else if (check == 'เข้าสู่ระบบสำเร็จ') {
+        //   showToastification(
+        //     context: context,
+        //     message: 'กำลังเข้าสู่ระบบ โปรดรอสักครู่',
+        //     type: ToastificationType.info,
+        //     style: ToastificationStyle.minimal,
+        //   );
+        //   await novelBox.put('loginsocial', true);
+        //   await novelBox.put('socialType', 'line');
+        //   BlocProvider.of<NovelBloc>(context).add(FetchNovels());
+        //   await Future.delayed(const Duration(seconds: 2));
+        //   await Future.delayed(const Duration(milliseconds: 500));
+        //   Navigator.pushNamedAndRemoveUntil(context, '/main', (route) => false);
+        // }
+      } else {
+        showToastification(
+          context: context,
+          message: 'โปรดยืนยันอีเมลกับline ก่อน',
+          type: ToastificationType.error,
+          style: ToastificationStyle.minimal,
+        );
+      }
+    } catch (e) {
+      showToastification(
+        context: context,
+        message: 'เกิดข้อผิดพลาดในการลงทะเบียน',
+        type: ToastificationType.error,
+        style: ToastificationStyle.minimal,
+      );
+    }
+    // return downloadImage(imgUrl);
   } on PlatformException catch (e) {
     print(e);
     switch (e.code.toString()) {
@@ -75,9 +171,10 @@ Future<String?> startLineLogin(BuildContext context) async {
   }
 }
 
-void logoutLine() async {
+Future<void> logoutLine() async {
   try {
     await LineSDK.instance.logout();
+    print("Logout Success");
   } on PlatformException catch (e) {
     print(e.message);
   }
