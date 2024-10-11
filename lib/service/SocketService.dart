8049af374da8453fc8706db77d82bee81405df0e
primer03 <panupong.person@gmail.com>
2024-10-11 16:02:14 +0700

    refactor: Update internet connection handling in SocketService.dart
    Refactor the `SocketService.dart` file to improve internet connection handling. The code now includes a `checkInternetConnection` function that uses the `connectivity_plus` package to check for internet connectivity. If there is no internet connection, the socket connection is not established and appropriate log messages are printed. This change enhances the reliability and user experience of the app by ensuring that the socket connection is only established when there is an active internet connection.
    
    Co-authored-by: [Author Name]

--- lib/service/SocketService.dart
+++ lib/service/SocketService.dart
@@ -1,16 +1,36 @@
 import 'dart:convert';
-
 import 'package:bloctest/main.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:logger/web.dart';
 import 'package:socket_io_client/socket_io_client.dart' as IO;
 import 'package:bloctest/models/user_model.dart' as userModel;
 
 IO.Socket? socket;
 
+Future<bool> checkInternetConnection() async {
+  final List<ConnectivityResult> connectivityResult =
+      await (Connectivity().checkConnectivity());
+  // ถ้า connectivityResult มีค่า none แสดงว่าไม่มีการเชื่อมต่ออินเทอร์เน็ต
+  print('Connectivity result: $connectivityResult');
+  // false แสดงว่าไม่มีการเชื่อมต่ออินเทอร์เน็ต
+  return connectivityResult.contains(ConnectivityResult.none) ? false : true;
+}
+
 void setupSocket() async {
   // ถ้า socket ไม่ใช่ null หรือยังไม่ได้เชื่อมต่อ ให้ตั้งค่าใหม่
   if (socket != null && socket!.connected) {
-    print('Socket is already connected');
+    Logger().i('Socket is already connected');
     return; // ถ้ามีการเชื่อมต่ออยู่แล้ว ไม่ต้องเชื่อมต่อใหม่
+  } else {
+    Logger().i('Socket is not connected $socket');
+  }
+
+  // Check internet connection
+  if (!await checkInternetConnection()) {
+    Logger().i('No internet connection. Cannot connect to socket');
+    return;
+  } else {
+    Logger().i('Internet connection available. Connecting to socket...');
   }
 
   socket = IO.io(
@@ -23,11 +43,12 @@ void setupSocket() async {
 
   // ฟังก์ชันที่ใช้เมื่อเชื่อมต่อสำเร็จ
   socket!.onConnect((_) async {
-    print('Connected');
-    print('User token: ${await novelBox.get('usertoken')}');
+    // print('Connected');
+    Logger().i('Socket connected');
+    // print('User token: ${await novelBox.get('usertoken')}');
     userModel.User user =
         userModel.User.fromJson(json.decode(await novelBox.get('user')));
-    print('User ID: ${user.userid}');
+    // print('User ID: ${user.userid}');
     socket!.emit('usermobile_online', user.userid);
   });
 
@@ -35,8 +56,21 @@ void setupSocket() async {
     print('Message from server: $data');
   });
 
-  socket!.onDisconnect((_) {
-    print('Disconnected');
+  socket!.onDisconnect((_) async {
+    // if (socket != null) {
+    //   if (socket == null) {
+    //     Logger().i('Socket disconnected and cleared');
+    //   }
+    // }
+    // if (!await checkInternetConnection()) {
+    //   Logger().i('No internet connection. Cannot reconnect to socket');
+    // }
+    if (!await checkInternetConnection()) {
+      Logger().i('Disconnected from socket. No internet connection');
+    } else {
+      Logger().i('Disconnected from socket. Reconnecting...');
+    }
+    Logger().e('Socket disconnected');
   });
 
   socket!.connect();
@@ -47,13 +81,21 @@ void disconnectSocket() {
     socket!.disconnect();
     socket!.clearListeners(); // Remove all listeners
     socket = null; // Clear socket reference
-    print('Socket disconnected and cleared');
+    if (socket == null) {
+      Logger().i('Socket disconnected and cleared');
+    }
   }
 }
 
-void reconnectSocket() {
+void reconnectSocket() async {
   if (socket == null || !socket!.connected) {
-    setupSocket(); // เรียกฟังก์ชัน setupSocket เพื่อเชื่อมต่อใหม่
-    print('Socket reconnected');
+    // Check internet connection before reconnecting
+    if (await checkInternetConnection()) {
+      print('Reconnecting to socket...');
+      setupSocket();
+      print('Socket reconnected');
+    } else {
+      print('No internet connection. Cannot reconnect to socket.');
+    }
   }
 }
