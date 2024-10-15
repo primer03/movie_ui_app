b1ad72240bba06a9dc3073ffad646d3c51f91065
primer03 <panupong.person@gmail.com>
2024-10-15 17:56:30 +0700

    feat: Add ChangeEmailPage for changing email functionality

--- lib/routes/app_router.dart
+++ lib/routes/app_router.dart
@@ -5,6 +5,7 @@ import 'package:bloctest/pages/cate/category_page.dart';
 import 'package:bloctest/pages/auth/login_page.dart';
 import 'package:bloctest/pages/detail/novel_detail.dart';
 import 'package:bloctest/pages/detail/novel_detail_new.dart';
+import 'package:bloctest/pages/email/change_email_page.dart';
 import 'package:bloctest/pages/password/change_password.dart';
 import 'package:bloctest/pages/profile/profile_page.dart';
 import 'package:bloctest/pages/reader/reader_page.dart';
@@ -80,9 +81,15 @@ class AppRouter {
           type: PageTransitionType.rightToLeft,
           curve: Curves.easeInOut,
         );
-      case '/changeemail':
+      case '/changePassword':
         return PageTransition(
           child: const ChangePasswordPage(),
+          type: PageTransitionType.rightToLeft,
+          curve: Curves.easeInOut,
+        );
+      case '/changeEmail':
+        return PageTransition(
+          child: const ChangeEmailPage(),
           type: PageTransitionType.rightToLeft,
           curve: Curves.easeInOut,
         );
