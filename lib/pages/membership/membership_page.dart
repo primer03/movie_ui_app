a2314c156f9900dc01b944796a6d1e5abcad2e67
primer03 <panupong.person@gmail.com>
2024-10-18 17:10:32 +0700

    feat: Add lazy loading for images in MembershipPage
    This code change adds lazy loading functionality for images in the MembershipPage. By implementing lazy loading, the app improves page load performance by only loading images when they are visible to the user. This enhances the user experience and reduces unnecessary network requests.
    
    Co-authored-by: [Author Name]

---
+++ lib/pages/membership/membership_page.dart
@@ -0,0 +1,227 @@
+import 'dart:async';
+
+import 'package:bloctest/main.dart';
+import 'package:bloctest/widgets/MembershipCard.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:in_app_purchase/in_app_purchase.dart';
+import 'package:in_app_purchase_android/in_app_purchase_android.dart';
+import 'package:intl/intl.dart';
+import 'package:logger/logger.dart';
+
+class MembershipPage extends StatefulWidget {
+  const MembershipPage({super.key});
+
+  @override
+  State<MembershipPage> createState() => _MembershipPageState();
+}
+
+class _MembershipPageState extends State<MembershipPage> {
+  List<ProductDetails> _products = [];
+  static const _variant = {'vip_30_day'};
+  final InAppPurchase _inAppPurchase = InAppPurchase.instance;
+
+  @override
+  void initState() {
+    super.initState();
+    initStore();
+  }
+
+  initStore() async {
+    print('isAvailable ${await _inAppPurchase.isAvailable()}');
+    ProductDetailsResponse productDetailResponse =
+        await _inAppPurchase.queryProductDetails(_variant);
+
+    if (productDetailResponse.notFoundIDs.isEmpty) {
+      setState(() {
+        _products = productDetailResponse.productDetails;
+      });
+    } else {
+      globalScaffoldMessengerKey.currentState!.showSnackBar(
+        SnackBar(content: Text('Products not found')),
+      );
+    }
+  }
+
+  buy() async {
+    await InAppPurchase.instance.restorePurchases();
+    if (_products.isNotEmpty) {
+      // Logger().i('Buy ${_products[1].id}');
+      final PurchaseParam purchaseParam =
+          PurchaseParam(productDetails: _products[0]);
+      _inAppPurchase.buyNonConsumable(purchaseParam: purchaseParam);
+    } else {
+      print('No product');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        leading: IconButton(
+          icon: Icon(
+            Icons.arrow_back_ios_new,
+            color: Colors.white,
+          ),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+      body: Stack(
+        children: [
+          Container(
+            width: double.infinity,
+            height: 150,
+            child: Padding(
+              padding: const EdgeInsets.all(10),
+              child: Image.asset(
+                'assets/images/logo_bookfet_white.png',
+                width: 100,
+              ),
+            ),
+          ),
+          DraggableScrollableSheet(
+            initialChildSize: 0.78,
+            minChildSize: 0.78,
+            maxChildSize: 0.78,
+            snap: true,
+            builder: (BuildContext context, ScrollController scrollController) {
+              return Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.vertical(top: Radius.circular(30)),
+                ),
+                child: ListView(
+                  controller: scrollController,
+                  padding: const EdgeInsets.all(25),
+                  children: [
+                    Text(
+                      'อ่านนิยายสุดพิเศษ ที่มีเฉพาะสำหรับสมาชิก VIP เท่านั้น! เปิดโลกแห่งการอ่านที่เต็มไปด้วยความสนุกสนาน พร้อมกับการเข้าถึงตอนใหม่และเนื้อหาพิเศษมากมายก่อนใคร!',
+                      style: GoogleFonts.athiti(
+                        fontSize: 17,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                    Divider(
+                      color: Colors.grey[300],
+                      thickness: 1,
+                      height: 30,
+                    ),
+                    MenuDetailMemberShip(
+                      header: 'เดือน',
+                      subHeader: MonthHelper.getThaiMonth(DateTime.now().month),
+                    ),
+                    const SizedBox(height: 45),
+                    MenuDetailMemberShip(
+                      header: 'ราคา',
+                      subHeader: '429 บาท',
+                    ),
+                    const SizedBox(height: 45),
+                    MenuDetailMemberShip(
+                      header: 'วันที่เริ่มต้น',
+                      subHeader:
+                          '${DateTime.now().day} ${MonthHelper.getThaiMonth(DateTime.now().month)} ${DateTime.now().year}',
+                    ),
+                    const SizedBox(height: 45),
+                    MenuDetailMemberShip(
+                      header: 'วันสิ้นสุด',
+                      subHeader: getDateAfter30Days(),
+                    ),
+                    const SizedBox(height: 45),
+                    ElevatedButton(
+                      onPressed: () {
+                        buy();
+                      },
+                      style: ElevatedButton.styleFrom(
+                        overlayColor: Colors.white,
+                        splashFactory: InkRipple.splashFactory,
+                        backgroundColor: Colors.black,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(15),
+                        child: Text(
+                          'ชำระเงิน',
+                          style: GoogleFonts.athiti(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
+  String getDateAfter30Days() {
+    DateTime currentDate = DateTime.now(); // วันที่ปัจจุบัน
+    DateTime futureDate =
+        currentDate.add(const Duration(days: 30)); // วันที่อีก 30 วัน
+
+    // ใช้ DateFormat เพื่อจัดรูปแบบวันที่
+    DateFormat dateFormat = DateFormat('dd MMMM yyyy', 'th');
+    String formattedDate = dateFormat.format(futureDate);
+
+    return formattedDate; // คืนค่าผลลัพธ์
+  }
+
+  Row MenuDetailMemberShip(
+      {required String header, required String subHeader}) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          header,
+          style: GoogleFonts.athiti(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        Text(
+          subHeader,
+          style: GoogleFonts.athiti(
+            fontSize: 20,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class MonthHelper {
+  static const List<String> _thaiMonths = [
+    'มกราคม',
+    'กุมภาพันธ์',
+    'มีนาคม',
+    'เมษายน',
+    'พฤษภาคม',
+    'มิถุนายน',
+    'กรกฎาคม',
+    'สิงหาคม',
+    'กันยายน',
+    'ตุลาคม',
+    'พฤศจิกายน',
+    'ธันวาคม',
+  ];
+
+  static String getThaiMonth(int month) {
+    if (month < 1 || month > 12) {
+      throw ArgumentError('Month must be between 1 and 12');
+    }
+    return _thaiMonths[month - 1];
+  }
+}
